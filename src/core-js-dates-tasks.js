13cbce0268c84693ccd4ff4cc12c651fd93aab01
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2025-02-25 12:56:34 +0300

    fix: isLeapYear test

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -377,7 +377,7 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  */
 function isLeapYear(date) {
   const year = date.getFullYear();
-  return !(year % 4);
+  return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
 module.exports = {
